1d4398b66fa0f33d1f428dee4b7fa6247f74888a
Gia Edgington <gia.edgington@gmail.com>
2020-04-14 13:12:16 -0400

    Serve image files statically

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+
 app.use(express.static(path.join(__dirname, 'public')));
+//image files are assumed to be on the root folder, need to add '/images' as what has been requested
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(
   session({
     secret: 'mysecrettext', 
